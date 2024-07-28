77f2972354736fe861533e8edebe771328a3cae6
lilfireflylil <oofaisalahmadioo@gmail.com>
2024-07-28 20:26:57 +0430

    handle 'media_type' = 'video' from API

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,14 +8,26 @@ function Main(props: MainProps) {
   const { data } = props;
 
   return (
-    <div className="imageContainer">
-      <img
-        src={data.hdurl}
-        alt={data.title || "Background Image"}
-        className="bgImage"
-        title={`Image Credit & Copyright: ${data.copyright?.trim() || "NASA"}`}
-      />
-    </div>
+    <>
+      {data.media_type === "image" ? (
+        <div className="imageContainer">
+          <img
+            src={data.hdurl || data.url}
+            alt={data.title || "Background Image"}
+            className="bgImage"
+            title={`Image Credit & Copyright: ${
+              data.copyright?.trim() || "NASA"
+            }`}
+          />
+        </div>
+      ) : data.media_type === "video" ? (
+        <div className="videoContainer">
+          <iframe src={data.url} className="bgVideo"></iframe>
+        </div>
+      ) : (
+        <p>unknown media type</p>
+      )}
+    </>
   );
 }
 
